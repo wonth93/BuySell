80ff6b857f283b417876ab644a0e7c9ba05dcb0a
Vanessa Power <vanessa.l.power@gmail.com>
2022-11-10 19:01:41 -0800

    seeding the tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- INSERT INTO users (name) VALUES ('Alice');
+-- INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (name, email, password)
+VALUES ('Admin User', 'admin@gmail.com', 'password'),
+('Non Admin', 'user@gmail.com', 'password');
+
