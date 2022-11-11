c41371f4f01cd06b25ab7970edbf760ca3bfd2b5
wonth93 <wongth93@gmail.com>
2022-11-11 05:53:46 +0000

    update drop table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,6 +2,8 @@
 -- INSERT INTO users (name) VALUES ('Alice');
 -- INSERT INTO users (name) VALUES ('Kira');
 
+DELETE FROM users;
+
 INSERT INTO users (name, email, password)
 VALUES ('Admin User', 'admin@gmail.com', 'password'),
 ('Non Admin', 'user@gmail.com', 'password');
