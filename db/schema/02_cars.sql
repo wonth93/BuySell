c8e512ba5642cd6b22b04938994219d787a5fdd6
wonth93 <wongth93@gmail.com>
2022-11-11 03:47:38 +0000

    update drop table

--- db/schema/02_cars.sql
+++ db/schema/02_cars.sql
@@ -1,6 +1,7 @@
 -- Drop and recreate Widgets table (Example)
 
 DROP TABLE IF EXISTS cars CASCADE;
+DROP TABLE IF EXISTS widgets;
 -- CREATE TABLE widgets (
 --   id SERIAL PRIMARY KEY NOT NULL,
 --   user_id INTEGER REFERENCES users(id),
