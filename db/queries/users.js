68f33f8e1693f6f77f052dd55a52daeaaf498dee
Vanessa Power <vanessa.l.power@gmail.com>
2022-11-10 18:25:24 -0800

    creating users and cars tables

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,10 +1,15 @@
-const db = require('../connection');
+const db = require("../connection");
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
 };
 
-module.exports = { getUsers };
+const getAllCars = () => {
+  return db.query("SELECT * FROM cars;").then((data) => {
+    return data.rows;
+  });
+};
+
+module.exports = { getUsers, getAllCars };
---
+++ db/schema/02_cars.sql
@@ -0,0 +1,33 @@
+-- Drop and recreate Widgets table (Example)
+
+DROP TABLE IF EXISTS cars CASCADE;
+-- CREATE TABLE widgets (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id),
+--   name VARCHAR(255) NOT NULL
+-- );
+
+
+CREATE TABLE cars (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  title VARCHAR(255) NOT NULL,
+  manufacturer VARCHAR(255) NOT NULL,
+  condition VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  price INTEGER  NOT NULL DEFAULT 0,
+  mileage INTEGER  NOT NULL DEFAULT 0,
+  year YEAR NOT NULL,
+  date_posted TIMESTAMP NOT NULL,
+
+  -- country VARCHAR(255) NOT NULL,
+  -- street VARCHAR(255) NOT NULL,
+  -- city VARCHAR(255) NOT NULL,
+  -- province VARCHAR(255) NOT NULL,
+  -- post_code VARCHAR(255) NOT NULL,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
