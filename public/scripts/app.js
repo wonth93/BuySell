8cecfbd7df5efc3dfe47457293e439075a000f3a
Vanessa Power <vanessa.l.power@gmail.com>
2022-11-11 13:15:21 -0800

    working on displaying data on the client side

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
       dataType: "json",
       success: (cars) => {
         console.log("data", cars);
-        //renderTweets(tweets);
+        renderCars(cars);
       },
       error: (err) => {
         console.log(`error: ${err}`);
@@ -18,47 +18,55 @@ $(document).ready(function () {
 
   loadCars();
 
-  // //create individual tweet
-  // const createTweetElement = function (tweetData) {
-  //   const { user, content, created_at } = tweetData;
-  //   const timePassed = timeago.format(created_at);
+  //create individual tweet
+  const createCarElement = function (carData) {
+    const {
+      title,
+      manufacturer,
+      condition,
+      thumbnail_photo_url,
+      mileage,
+      price,
+    } = carData;
+    // const timePassed = timeago.format(created_at);
 
-  //   const escape = function (str) {
-  //     let div = document.createElement("div");
-  //     div.appendChild(document.createTextNode(str));
-  //     return div.innerHTML;
-  //   };
+    // const escape = function (str) {
+    //   let div = document.createElement("div");
+    //   div.appendChild(document.createTextNode(str));
+    //   return div.innerHTML;
+    // };
 
-  //   const $tweet = $(`<article class="tweet">
-  //   <header>
-  //     <div>
-  //       <img src="${user.avatars}" class="avatar"></img>
-  //       <span class="tweet-name">${user.name}</span>
-  //     </div>
-  //     <span class="tweet-username">${user.handle}</span>
-  //   </header>
-  //   <div><p>${escape(content.text)}</p></div>
-  //   <footer>
-  //     <span class="tweet-time">${timePassed}</span>
-  //     <span class="tweet-icons">
-  //       <i class="fa-solid fa-flag"></i>
-  //       <i class="fa-sharp fa-solid fa-retweet"></i>
-  //       <i class="fa-sharp fa-solid fa-heart"></i>
-  //     </span>
-  //   </footer>
-  // </article>`);
+    const $car = $(`<article>
+    <div><img src="${thumbnail_photo_url}" width="200"></img></div>
+    <div>
+      <h3>${title}</h3>
+      <ul>
+        <li>${manufacturer}</li>
+        <li>${condition}n</li>
+        <li>${price}</li>
+        <li>${mileage}</li>
+      </ul>
+      <footer>
+        <ul>
+          <li>Learn More</li>
+          <li>Favourite</li>
+        </ul>
+      </footer>
+  </div>
+  </article>`);
 
-  //   return $tweet;
-  // };
+    return $car;
+  };
 
-  // //rendering all of the tweets after grabbing them via ajax into the tweet container
-  // const renderTweets = function (tweets) {
-  //   $("#tweets-container").empty();
-  //   for (let tweet of tweets) {
-  //     const $tweet = createTweetElement(tweet);
-  //     $("#tweets-container").prepend($tweet);
-  //   }
-  // };
+  //rendering all of the tweets after grabbing them via ajax into the tweet container
+  const renderCars = (cars) => {
+    $("#cars-container").empty();
+    console.log(cars);
+    for (let car of cars) {
+      const $car = createCarElement(car);
+      $("#cars-container").prepend($car);
+    }
+  };
 
   // //tweet form - posting to the tweets data and handling errors
   // const form = $(".tweet-form");
