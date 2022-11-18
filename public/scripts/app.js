af5776c782fee78acb1d7c14a9e146c073bab574
wonth93 <wongth93@gmail.com>
2022-11-18 08:18:08 +0000

    clean up app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,26 @@
 // Client facing scripts here
 $(document).ready(function () {
-  //grabbing all of the tweets via ajax
 
+  // header link element
   const $header = `<ul class="header-links">
-    <li id="home">Home</li>
-    <li id="my-listings">My Listings</li>
-    <li id="my-favourites">My Favourites</li>
-    <li id="create-listing">Create a New Listing</li>
-    <li id="my-messages">My messages</li>
-    <form class="logout-button" method="POST" action="/api/users/logout"><button>Logout</button></form>
+    <li id="home" style="cursor: pointer">Home</li>
+    <li id="my-listings" style="cursor: pointer">My Listings</li>
+    <li id="my-favourites" style="cursor: pointer">My Favourites</li>
+    <li id="create-listing" style="cursor: pointer">Create a New Listing</li>
+    <li id="my-messages" style="cursor: pointer">My messages</li>
+    <form class="logout-button" method="POST" action="/api/users/logout"><button style="cursor: pointer">Logout</button></form>
   </ul>`;
 
   $("#page-header").prepend($header);
 
   const $main = $("#main-content");
 
-  //Loading the homepage with all the listings///////
 
+  // ********************** Load the homepage with all the listings ********************** //
   const loadHomepage = function () {
     $main.empty();
+
+    // Filter element
     $main.append(` <h2 class="section-title">All Cars</h2>
     <h4 class="filter-title">Filter by price</h4>
     <form
@@ -42,30 +44,29 @@ $(document).ready(function () {
       </div>
 
       <div class="filter-car-form__field-wrapper">
-        <button class="submit-filter" type="submit">Search</button>
-        <span class="clear-filter">Clear Filter</span>
+        <button class="submit-filter" type="submit" style="cursor: pointer">Search</button>
+        <span class="clear-filter" style="cursor: pointer">Clear Filter</span>
         <!-- <a id="filter-car-form__cancel" href="#">Cancel</a> -->
       </div>
     </form>
     <section id="error-message"></section>
     <section id="cars-container"></section>`);
 
+    // Function that loads all cars to home page function
     const loadCars = (params) => {
       let url = "/cars";
+
       if (params) {
         url = `/cars/price?${params}`;
       }
-      //alert(url);
 
       $.ajax({
         url: url,
         method: "GET",
         dataType: "json",
         success: (carsObject) => {
-          //console.log(typeof carsObject);
           const { cars } = carsObject;
           if (cars.length === 0) {
-            //$("#error-message").empty();
             $("#error-message").append(`<h3>Sorry, no cars available</h3>`);
           }
           renderCars(cars);
@@ -78,7 +79,7 @@ $(document).ready(function () {
 
     loadCars();
 
-    //create individual tweet
+    // Function that create individual car element on home page function
     const createCarElement = (carData) => {
       const {
         id,
@@ -88,16 +89,9 @@ $(document).ready(function () {
         thumbnail_photo_url,
         mileage,
         price,
-        description,
         active,
+        year,
       } = carData;
-      // const timePassed = timeago.format(created_at);
-
-      // const escape = function (str) {
-      //   let div = document.createElement("div");
-      //   div.appendChild(document.createTextNode(str));
-      //   return div.innerHTML;
-      // };
 
       const $car = $(`<article class="car active-${active}" id=${id}>
       <div><img src="${thumbnail_photo_url}" class="car-image"></img></div>
@@ -106,6 +100,7 @@ $(document).ready(function () {
         <div class="car-details">
         <ul>
           <li>Manufacturer: ${manufacturer}</li>
+          <li>Year: ${year}</li>
           <li>Condition: ${condition}</li>
           <li>Price: $${price}</li>
           <li>Mileage: ${mileage} miles</li>
@@ -113,8 +108,8 @@ $(document).ready(function () {
         <footer class="car-actions">
           <ul>
             <li class="sold">SOLD</li>
-            <li><button class="learn-more">Learn More</button></li>
-            <li><form class="add-fav" method="POST" action="/api/users/myFavourites/${id}/add"><button class="add-fav-button">Favourite</button></form><li>
+            <li><button class="learn-more" style="cursor: pointer">Learn More</button></li>
+            <li><form class="add-fav" method="POST" action="/api/users/myFavourites/${id}/add"><button class="add-fav-button" style="cursor: pointer">Favourite</button></form><li>
           </ul>
         </footer>
         </div>
@@ -124,57 +119,48 @@ $(document).ready(function () {
       return $car;
     };
 
-    //rendering all of the tweets after grabbing them via ajax into the tweet container
+    // Function that renders all cars after grabbing them via ajax into the page
     const renderCars = (cars) => {
       $("#cars-container").empty();
-      //console.log(cars);
       for (let car of cars) {
         const $car = createCarElement(car);
         $("#cars-container").prepend($car);
       }
     };
 
-    //Adding a car to favourites
-    $("#cars-container").on("click", ".add-fav", function (e) {
-      const id = $(this).closest("article").attr("id");
-      alert(`Added car #${id} to your favourites`);
-    });
-
+    // On click event to load single car info
     $("#cars-container").on("click", ".learn-more", function () {
-      //loadSingleCarPage();
       $("#error-message").empty();
       const id = $(this).closest("article").attr("id");
-      //alert(`rendering message form about ${id} car`);
       loadSingleCarPage(id);
     });
 
+    // On click event to clear filter and reload all cars
     $("#filter-car-form").on("click", ".clear-filter", function (event) {
-      //event.preventDefault();
-      //const data = $(this).serialize();
-      //alert(data);
       $("#error-message").empty();
       loadCars();
     });
 
+    // On submit event to load filtered list of cars
     $("#filter-car-form").on("submit", function (event) {
       event.preventDefault();
       $("#error-message").empty();
-      //$("#error-message").empty();
       const data = $(this).serialize();
-      //alert(data);
       loadCars(data);
     });
   };
 
-  //Calling load homepage when index loads
+  // Calling load homepage when index loads
   loadHomepage();
 
-  ///////////////////////Loading the my listings page/////////////////////////////
+
+  // ********************** Load my listings page ********************** //
   const loadMyListings = function () {
     $main.empty();
     $main.append(` <h2 class="section-title">My listings</h2>
     <section id="cars-container"></section>`);
 
+    // Function that loads all cars that are list by the seller function
     const loadListings = () => {
       $.ajax({
         url: "/api/users/myListings",
@@ -192,7 +178,7 @@ $(document).ready(function () {
 
     loadListings();
 
-    //create individual tweet
+    // Function that creates individual car element on the my listings page
     const createCarElement = (carData) => {
       console.log(carData);
       const {
@@ -203,16 +189,9 @@ $(document).ready(function () {
         thumbnail_photo_url,
         mileage,
         price,
-        description,
+        year,
         active,
       } = carData;
-      // const timePassed = timeago.format(created_at);
-
-      // const escape = function (str) {
-      //   let div = document.createElement("div");
-      //   div.appendChild(document.createTextNode(str));
-      //   return div.innerHTML;
-      // };
 
       const $car = $(`<article class="car active-${active}" id=${id}>
       <div><img src="${thumbnail_photo_url}" class="car-image" ></img></div>
@@ -221,16 +200,16 @@ $(document).ready(function () {
         <div class="car-details">
         <ul>
           <li>Manufacturer: ${manufacturer}</li>
+          <li>Year: ${year}</li>
           <li>Condition: ${condition}</li>
           <li>Price: $${price}</li>
           <li>Mileage: ${mileage} miles</li>
-          <li>Description: ${description}</li>
         </ul>
         <footer class="car-actions">
           <ul>
           <li class="sold">SOLD</li>
-          <li><form class="delete-listing" method="POST" action="/cars/${id}/delete"><button class="delete-listing-button">Delete this listing</button></form></li>
-          <li><form class="mark-sold" method="POST" action="/cars/${id}/sold"><button class="mark-sold-button">Mark As Sold</button></form></li>
+          <li><form class="delete-listing" method="POST" action="/cars/${id}/delete"><button class="delete-listing-button" style="cursor: pointer">Delete this listing</button></form></li>
+          <li><form class="mark-sold" method="POST" action="/cars/${id}/sold"><button class="mark-sold-button" style="cursor: pointer">Mark As Sold</button></form></li>
           </ul>
         </footer>
         </div>
@@ -240,7 +219,7 @@ $(document).ready(function () {
       return $car;
     };
 
-    //rendering all of the tweets after grabbing them via ajax into the tweet container
+    // Function that renders all cars after grabbing them via ajax into my listings page
     const renderListings = (cars) => {
       $("#cars-container").empty();
       console.log(cars);
@@ -250,58 +229,22 @@ $(document).ready(function () {
       }
     };
 
-    // //Delete a listing
-    // $("#cars-container").on("click", ".delete-listing", function (e) {
-    //   const id = $(this).closest("article").attr("id");
-    //   // const url = `api/users/myListings/${id}/delete`;
-    //   //alert(`Successfully deleted Car Listing  #${id}`);
-    //   //loadHomepage();
-    // });
-
-    //Delete a listing
-    // $(".delete-listing").on("submit", function (e) {
-    //   e.preventDefault();
-    //   const id = $(this).closest("article").attr("id");
-    //   alert(`Successfully deleted Car Listing  #${id}`);
-    //   loadListings();
-    //   //loadHomepage();
-    // });
-    $("#cars-container").on("click", ".delete-listing", function (e) {
-      const id = $(this).closest("article").attr("id");
-      //const url = `api/users/myFavourites/${id}/delete`;
-      alert(`Deleted car #${id} from your listings`);
-    });
-
-    $("#cars-container").on("click", ".mark-sold", function (e) {
-      const id = $(this).closest("article").attr("id");
-      //const url = `api/users/myFavourites/${id}/delete`;
-      alert(`Marked car #${id} as sold`);
-    });
-
-    //Mark a listing as sold
-
-    // $("#cars-container").on("click", ".mark-sold", function (e) {
-    //   const id = $(this).closest("article").attr("id");
-    //   const url = `api/users/myListings/${id}`;
-    //   alert(`send a post/update request to ${url} then call loadListings()`);
-    //   loadSoldListing();
-    //   // loadListings();
-    // });
   };
 
-  /////////////////// Loading the my favourites//////////////
+
+  // ********************** Loading the my favourites ********************** //
   const loadMyFavs = function () {
     $main.empty();
     $main.append(` <h2 class="section-title">My favourites</h2>
     <section id="cars-container"></section>`);
 
+    // Function that loads users' favourite cars
     const loadFavs = () => {
       $.ajax({
         url: "/api/users/myFavourites",
         method: "GET",
         dataType: "json",
         success: (carsObject) => {
-          //console.log(typeof carsObject);
           const { cars } = carsObject;
           renderFavs(cars);
         },
@@ -313,7 +256,7 @@ $(document).ready(function () {
 
     loadFavs();
 
-    //create individual tweet
+    // Function that creates individual car element on the my favourites page
     const createCarElement = (carData) => {
       const {
         id,
@@ -324,16 +267,8 @@ $(document).ready(function () {
         thumbnail_photo_url,
         mileage,
         price,
-        description,
         active,
       } = carData;
-      // const timePassed = timeago.format(created_at);
-
-      // const escape = function (str) {
-      //   let div = document.createElement("div");
-      //   div.appendChild(document.createTextNode(str));
-      //   return div.innerHTML;
-      // };
 
       const $car = $(`<article class="car active-${active}"id=${id}>
       <div><img src="${thumbnail_photo_url}" class="car-image"></img></div>
@@ -349,8 +284,8 @@ $(document).ready(function () {
         <footer class="car-actions">
           <ul>
           <li class="sold">SOLD</li>
-            <li><button class="learn-more">Learn More</button></li>
-            <li><form class="remove-fav" method="POST" action="/api/users/myFavourites/${car_fav_id}/delete"><button type="submit">Remove from favourites</button></form><li>
+            <li><button class="learn-more" style="cursor: pointer">Learn More</button></li>
+            <li><form class="remove-fav" method="POST" action="/api/users/myFavourites/${car_fav_id}/delete"><button type="submit" style="cursor: pointer">Remove from favourites</button></form><li>
           </ul>
         </footer>
         </div>
@@ -360,36 +295,29 @@ $(document).ready(function () {
       return $car;
     };
 
-    //rendering all of the tweets after grabbing them via ajax into the tweet container
+    // Function that renders all cars after grabbing them via ajax into my favourites page
     const renderFavs = (cars) => {
       $("#cars-container").empty();
-      //console.log(cars);
       for (let car of cars) {
         const $car = createCarElement(car);
         $("#cars-container").prepend($car);
       }
     };
 
-    $("#cars-container").on("click", ".remove-fav", function (e) {
-      const id = $(this).closest("article").attr("id");
-      //const url = `api/users/myFavourites/${id}/delete`;
-      alert(`Deleted car #${id} from Favourites`);
-    });
-
     $("#cars-container").on("click", ".learn-more", function () {
-      //loadSingleCarPage();
       const id = $(this).closest("article").attr("id");
-      //alert(`rendering message form about ${id} car`);
       loadSingleCarPage(id);
     });
   };
 
-  /////////Load the createListing form//////////////////////
+
+  // ********************** Load the createListing form ********************** //
   const loadCreateListing = function () {
     $main.empty();
     $main.append(`<h3 class="section-title">Sell your car</h3>
     <section id="new-car-container"></section>`);
 
+    // Create new listing form element
     const $createListingForm = $(`
     <form action="cars/add" method="post" id="new-car-form">
 
@@ -439,7 +367,7 @@ $(document).ready(function () {
       </div>
 
       <div class="new-car-form-wrapper">
-        <button type="submit">Create</button>
+        <button type="submit" style="cursor: pointer">Create</button>
       </div>
 
     </form>
@@ -448,27 +376,27 @@ $(document).ready(function () {
     $("#new-car-container").append($createListingForm);
   };
 
-  ///////// Rendering an individual car page //////////////
+
+  // ********************** Rendering an individual car page ********************** //
   const loadSingleCarPage = function (id) {
     $main.empty();
     $main.append(`<h3 class="section-title">View Car Details</h3>`);
     const url = `/cars/${id}`;
 
+    // Loads individual car info
     $.ajax({
       url: url,
       method: "GET",
       dataType: "json",
       success: (carObject) => {
-        //console.log(typeof carsObject);
-        // const { cars } = carsObject;
         createSingleCar(carObject);
-        //console.log(carObject);
       },
       error: (err) => {
         console.log(`error: ${err}`);
       },
     });
 
+    // // Function that creates individual car info element
     const createSingleCar = function (carObject) {
       const {
         id,
@@ -497,14 +425,14 @@ $(document).ready(function () {
               <li>Mileage: ${mileage}</li>
               <li>Description: ${description}</li>
             </ul>
-            <form class="add-fav" method="POST" action="/api/users/myFavourites/${id}/add"><button>Favourite</button></form>
+            <form class="add-fav" method="POST" action="/api/users/myFavourites/${id}/add"><button style="cursor: pointer">Favourite</button></form>
           </div>
           <footer class="single-car-message">
           <hr />
             <h3>Message the seller about this car</h3>
             <form class="send-message" method="POST" action="api/users/myMessages/${seller_id}/${id}/add">
               <textarea name="text" class="feedback-input" placeholder="Message here"></textarea>
-              <button type="submit">Send Message</button>
+              <button type="submit" style="cursor: pointer">Send Message</button>
               </form>
             </footer>
           </div>
@@ -520,22 +448,22 @@ $(document).ready(function () {
     };
   };
 
-  /////////////Loading messages page/////////////////////////
+
+  // ********************** Loading messages page ********************** //
   const loadMyMessages = function () {
     $main.empty();
     $main.append(`<h2 class="section-title">My Messages</h2>
     <section id="messages-container"></section>`);
 
+    // Function that loads users' messages
     const loadMessages = () => {
       $.ajax({
         url: "/api/users/myMessages",
         method: "GET",
         dataType: "json",
         success: (messagesObject) => {
-          //console.log(typeof carsObject);
           const { messages } = messagesObject;
           renderMessages(messages);
-          //console.log(messages);
         },
         error: (err) => {
           console.log(`error: ${err}`);
@@ -545,11 +473,10 @@ $(document).ready(function () {
 
     loadMessages();
 
+    // Function that create message element
     const createMessageElement = (singleMessage) => {
       const {
-        id,
         sender_id,
-        receiver_id,
         car_id,
         date_sent,
         message,
@@ -575,7 +502,7 @@ $(document).ready(function () {
           class="feedback-input"
           placeholder="Comment"
         ></textarea>
-        <button type="submit">Send Message</button>
+        <button type="submit" style="cursor: pointer">Send Message</button>
       </form>
     </div>
   </article>`);
@@ -583,6 +510,7 @@ $(document).ready(function () {
       return $message;
     };
 
+    // // Function that renders all messages after grabbing them via ajax into my messages page
     const renderMessages = (messages) => {
       $("#messages-container").empty();
       for (let message of messages) {
@@ -590,16 +518,10 @@ $(document).ready(function () {
         $("#messages-container").prepend($message);
       }
     };
-    // const renderCars = (cars) => {
-    //   $("#cars-container").empty();
-    //   //console.log(cars);
-    //   for (let car of cars) {
-    //     const $car = createCarElement(car);
-    //     $("#cars-container").prepend($car);
-    //   }
   };
 
-  //////////////Managing clicks on header to load different "pages"/////////
+  
+  // ********************** Managing clicks on header to load different "pages" ********************** //
   $("header").on("click", "#home", () => {
     loadHomepage();
   });
@@ -620,15 +542,4 @@ $(document).ready(function () {
     loadMyMessages();
   });
 
-  /////////////Managing clicks on message seller button/////////
-  // $("#cars-container").on("click", ".learn-more", function () {
-  //   //loadSingleCarPage();
-  //   const id = $(this).closest("article").attr("id");
-  //   //alert(`rendering message form about ${id} car`);
-  //   loadSingleCarPage(id);
-  // });
-
-  // $("header").on("click", ".logout-button", () => {
-  //   loadMyMessages();
-  // });
 });
